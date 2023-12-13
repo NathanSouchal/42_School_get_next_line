35e41f5940d505c0b56f5b6466f24564aa0d7552
Nathan Souchal <nsouchal@z3r13p3.42lyon.fr>
2023-12-13 16:05:46 +0100

    bonus commentes

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,24 +6,24 @@
 /*   By: nsouchal <nsouchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 10:41:41 by nsouchal          #+#    #+#             */
-/*   Updated: 2023/12/13 12:55:43 by nsouchal         ###   ########.fr       */
+/*   Updated: 2023/12/13 16:03:01 by nsouchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+// #ifndef GET_NEXT_LINE_BONUS_H
+// # define GET_NEXT_LINE_BONUS_H
 
-# include <unistd.h>
-# include <stdlib.h>
+// # include <unistd.h>
+// # include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4
-# endif
+// # ifndef BUFFER_SIZE
+// #  define BUFFER_SIZE 4
+// # endif
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(char *str);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *s);
-void	*ft_memmove(void *dest, void *src, size_t n);
+// char	*get_next_line(int fd);
+// size_t	ft_strlen(char *str);
+// char	*ft_strjoin(char *s1, char *s2);
+// char	*ft_strdup(char *s);
+// void	*ft_memmove(void *dest, void *src, size_t n);
 
-#endif
+// #endif
